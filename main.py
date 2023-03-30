cb96d58dc9079b8377f865f5543119c6f7ed8cad
Nazarii Arshynskyi <nazar.arshynskyi@gmail.com>
2023-03-30 14:13:58 +0300

    update main

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ data = {}
 
 with open(filename, 'r') as file:
     for line in file:
-        country, year, population = line.strip().split(',')
-        year = int(year)
-        population = int(population)
+        country, year_str, population_str = line.strip().split(',')
+        year = int(year_str)
+        population = int(population_str)
         if country not in data:
             data[country] = {year: population}
         else:
