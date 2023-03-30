3429e46b633becaecf6dc515203649378c46e23a
Nazarii Arshynskyi <nazar.arshynskyi@gmail.com>
2023-03-30 14:03:14 +0300

    added with function

---
+++ main.py
@@ -0,0 +1,12 @@
+filename = "population.txt"
+data = {}
+
+with open(filename, 'r') as file:
+    for line in file:
+        country, year, population = line.strip().split(',')
+        year = int(year)
+        population = int(population)
+        if country not in data:
+            data[country] = {year: population}
+        else:
+            data[country][year] = population
