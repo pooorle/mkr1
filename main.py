9e945340645dbfa806810f6fa02afb2b89a82b75
Nazarii Arshynskyi <nazar.arshynskyi@gmail.com>
2023-03-30 14:20:36 +0300

    updated check and population, added function

--- main.py
+++ main.py
@@ -10,4 +10,11 @@ with open(filename, 'r') as file:
             data[country] = {year: population}
         else:
             data[country][year] = population
-            
+
+for country in data:
+    years = sorted(data[country].keys())
+    for i in range(len(years)-1):
+        year1 = years[i]
+        year2 = years[i+1]
+        population_change = data[country][year2] - data[country][year1]
+        print(f"{country}: {year1} - {year2}: {population_change}")
